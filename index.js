2fab20414b21e015b064370f67854c7f5795f7a7
Silvana <skalliny@my.bcit.ca>
2020-09-08 21:00:18 -0700

    bug fix

--- index.js
+++ index.js
@@ -8,17 +8,14 @@ const { profileEnd } = require('console');
 
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/public')));
 
-
-if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.get('*', (req,res) =>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
-}
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
